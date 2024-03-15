310af439876a80dae1de26e698770e539a2823c1
Ahmed Nagah <ahmednagah031220@gmail.com>
2024-03-15 03:08:19 +0200

    2-file edit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
+import sys
+
+def main():
+    # Get the number of arguments excluding the script name
+    args_count = len(sys.argv) - 1
+    # Choose the right label for argument/arguments based on the count
+    arg_label = "argument" if args_count == 1 else "arguments"
+
+    # Print the number of arguments
+    print(f"Number of {arg_label}{'.' if args_count == 0 else ':'}", args_count)
+
+    # If there are arguments, print them
+    if args_count > 0:
+        print()  # Print a new line before listing the arguments
+        for i, arg in enumerate(sys.argv[1:], start=1):  # Start counting from 1
+            print(f"{i}: {arg}")
+
+# Check if the script is being run directly or imported
 if __name__ == "__main__":
-    import sys
-
-    lenght = len(sys.argv) - 1
-    if lenght == 0:
-        print("0 arguments.")
-    elif lenght == 1:
-        print("{} argument:".format(length))
-    else:
-        print("{} arguments:".format(lenght))
-
-    for i in range(lenght):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    main()
