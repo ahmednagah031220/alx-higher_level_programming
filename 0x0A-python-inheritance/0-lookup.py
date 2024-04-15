7903754ec76866d801e193dc7fd168281d28b526
Ahmed Nagah <ahmednagah031220@gmail.com>
2024-04-15 22:19:53 +0200

    0 task edit

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
-"""
-Contains the lookup function
-"""
+"""Contains the lookup function"""
+
 def lookup(obj):
     """returns a list of available attributes and methods of an object"""
     return dir(obj)
