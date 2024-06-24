abd5659f2b781663dd3e06f036afeb0f363c4274
Ahmed Nagah <ahmednagah031220@gmail.com>
2024-06-24 22:04:04 +0300

    update 0 and 1

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,35 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
+"""
+Models used in the task
+"""
+from sys import argv
 import MySQLdb
-import sys
+
+
+def main():
+    """
+    the main function for the porcess
+    """
+    db_user = argv[1]
+    db_password = argv[2]
+    db_db = argv[3]
+    db_host = "localhost"
+    db_port = 3306
+
+    try:
+        conn = MySQLdb.connect(host=db_host, port=db_port,
+                               user=db_user, passwd=db_password,
+                               db=db_db, charset="utf8")
+        cursor = conn.cursor()
+        cursor.execute("SELECT id, name FROM states ORDER BY id ASC")
+        query_rows = cursor.fetchall()
+        for row in query_rows:
+            print(row)
+        cursor.close()
+        conn.close()
+    except Exception:
+        pass
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
+    main()
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,17 +1,39 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
+"""
+Module used in the task
+"""
+from sys import argv
 import MySQLdb
-import sys
+
+
+def main():
+    """
+    Function of the models
+    """
+    db_user = argv[1]
+    db_password = argv[2]
+    db_db = argv[3]
+    db_host = "localhost"
+    db_port = 3306
+
+    try:
+        query_string = "SELECT * FROM states WHERE CONVERT(name USING Latin1)"
+        query_string += " COLLATE Latin1_General_CS"
+        query_string += " LIKE 'N%'"
+        connection = MySQLdb.connect(host=db_host, port=db_port,
+                                     user=db_user, passwd=db_password,
+                                     db=db_db, charset="utf8")
+        try:
+            cursor = connection.cursor()
+            cursor.execute(query_string)
+            query_rows = cursor.fetchall()
+            for row in query_rows:
+                print(row)
+        finally:
+            cursor.close()
+    finally:
+        connection.close()
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("""SELECT * FROM states WHERE name
-                LIKE BINARY 'N%' ORDER BY states.id""")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
+    main()
